284be7bf2b77c72e5dc57eaa7d34ca3379243ced
Sameera Viraj <sameera.emedia@gmail.com>
2020-05-03 16:49:21 +0530

    category crud done

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem 'mysql2', '~>0.5.3'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,48 @@
 class CategoriesController < ApplicationController
+    
+    def index
+        @categories = Category.all
+    end
+    
     def new
-    end    
+        @category = Category.new
+    end
+    
+    def edit
+        @category = Category.find(params[:id])
+    end
 
     def create
-        render plain: params[:category].inspect
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to @category
+        else 
+            render 'new'
+        end         
     end
+
+    def update
+        @category = Category.find(params[:id])
+
+        if @category.update(category_params)
+            redirect_to @category
+        else 
+            render 'edit'
+        end
+    end        
+
+    def show
+        @category = Category.find(params[:id])
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_path
+    end
+
+    private
+        def category_params
+            params.require(:category).permit(:name,:description)
+        end
 end
